a12f6bbc1875e966078bf86627ba481fc3e15773
xiaoxin <3115242940@qq.com>
2026-06-02 19:10:10 +0800

    comment

--- src/api/documentApi.js
+++ src/api/documentApi.js
@@ -4,5 +4,9 @@ export const documentApi = {
   getAll: () => request.get('/document/getAll/'),
   detail: (id) => request.get('/document/detail/', { params: { id } }),
   publish: (data) => request.post('/document/publish/', data),
-  remove: (id) => request.delete(`/document/remove/${id}/`)
+  remove: (id) => request.delete(`/document/remove/${id}/`),
+  // comments
+  getComments: (documentId) => request.get('/document/comment/list/', { params: { document_id: documentId } }),
+  addComment: (data) => request.post('/document/comment/add/', data),
+  deleteComment: (commentId) => request.delete(`/document/comment/delete/${commentId}/`),
 }
